d839e926dc91a3911d30f744f97001f25ff94382
Felix Parker <flixpar@gmail.com>
2018-08-01 13:36:44 -0400

    updated server to send and recieve messages over socketio

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import url_for, render_template
 
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 @app.route('/videos')
@@ -12,3 +16,15 @@ def index():
 def video(vid_id=None):
     vid_path = vid_id
     return render_template("video.html", vid_path=vid_path)
+
+@socketio.on('get_frames')
+def get_frames(vid_id):
+    frame_list = ["a", "b", "c"]
+    emit("recieve_frames", frame_list, json=True)
+
+@socketio.on('update_segm')
+def get_frames(vid_id, data):
+    print('received: ' + str(data))
+
+if __name__ == '__main__':
+    socketio.run(app)
