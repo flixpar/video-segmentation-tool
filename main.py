1e96fcd9208f001bba397d91e6f33da5bec23596
Felix Parker <flixpar@gmail.com>
2018-08-02 13:40:08 -0400

    finished server implementation v1

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from flask_socketio import send, emit
 import os
 import glob
 
+from tinydb import TinyDB, Query
+
+
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+db = TinyDB('db.json')
+
 
 @app.route('/')
 @app.route('/videos')
@@ -21,8 +27,16 @@ def video(vid_id):
     return render_template("video.html", vid_path=vid_path, vid_id=vid_id)
 
 @socketio.on('update_segm')
-def get_frames(vid_id, data):
-    print('received: ' + str(data))
+def update_segm(vid_id, data):
+    db.upsert({"vid_id": vid_id, "data": data}, Query().vid_id==vid_id)
+
+@socketio.on('query_segm')
+def send_segm(vid_id):
+    vid = db.search(Query().vid_id == vid_id)
+    if vid:
+        socketio.emit("get_segm", vid[0]["data"])
+    else:
+        socketio.emit("get_segm", [])
 
 if __name__ == '__main__':
     socketio.run(app)
